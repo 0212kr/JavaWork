c85fb36b1fb76988a1c49206f7754fbf1ab6af5e
0212kr <0212kr@gmail.com>
2020-05-06 10:26:47 +0900

    200506-1

--- Lec32_WebCrawl/src/com/lec/java/crawl01/Crawl00Main.java
+++ Lec32_WebCrawl/src/com/lec/java/crawl01/Crawl00Main.java
@@ -7,7 +7,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+ 
 public class Crawl00Main {
 
 	public static void main(String[] args) throws IOException {
